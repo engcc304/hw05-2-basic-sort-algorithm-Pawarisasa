207e39d8007467cf2b26e3cad640915a4b40197a
pawarisa <pawarisasangha15@gmail.com>
2023-08-16 23:49:46 +0700

    hw05.3

--- Homework/HW05/hw05-3-number-to-text.cpp
+++ Homework/HW05/hw05-3-number-to-text.cpp
@@ -16,30 +16,114 @@
         Four
 */
 #include <stdio.h>
+#include <iostream>
+using namespace std;
 int main()
 {
-    int a = 0, b = 0, c = 0 ;
-    printf("Input[1] ") ; scanf( "%d", &a ) ;
-    printf("Input[2] ") ; scanf( "%d", &b ) ;
-    printf("Input[3] ") ; scanf( "%d", &c ) ;
-    
-    if( a > b && a > c ) {
-        printf( "%d ", a ) ;
-        if( b > c ) printf( "%d %d ", b,c ) ;
-        else printf( "%d %d ", c,b ) ;
+    int num = 0 ;
+    printf("Please Enter your number : ") ;
+    scanf("%d", &num) ;
+    if (num > -1 && num < 31)
+    {
+        switch (num)
+        {
+        case 0:
+            cout << "Zero" ;
+            break;
+        case 1:
+            cout << "One";
+            break;
+        case 2:
+            cout << "Two";
+            break;
+        case 3:
+            cout << "Three";
+            break;
+        case 4:
+            cout << "Four";
+            break;
+        case 5:
+            cout << "Five";
+            break;
+        case 6:
+            cout << "Six";
+            break;
+        case 7:
+            cout << "Seven";
+            break;
+        case 8:
+            cout << "Eight";
+            break;
+        case 9:
+            cout << "Nine";
+            break;
+        case 10:
+            cout << "Ten";
+            break;
+        case 11:
+            cout << "Eleven";
+            break;
+        case 12:
+            cout << "Twelve";
+            break;
+        case 13:
+            cout << "Thirteen";
+            break;
+        case 14:
+            cout << "Fourteen";
+            break;
+        case 15:
+            cout << "Fifteen";
+            break;
+        case 16:
+            cout << "Sixteen";
+            break;
+        case 17:
+            cout << "Seventeen";
+            break;
+        case 18:
+            cout << "Eighteen";
+            break;
+        case 19:
+            cout << "Nineteen";
+            break;
+        case 20:
+            cout << "Twenty";
+            break;
+        case 21:
+            cout << "Twenty One";
+            break;
+        case 22:
+            cout << "Twenty Two";
+            break;
+        case 23:
+            cout << "Twenty Three";
+            break;
+        case 24:
+            cout << "Twenty Four";
+            break;
+        case 25:
+            cout << "Twenty Five";
+            break;
+        case 26:
+            cout << "Twenty Six";
+            break;
+        case 27:
+            cout << "Twenty Seven";
+            break;
+        case 28:
+            cout << "Twenty Eight";
+            break;
+        case 29:
+            cout << "Twenty Nine";
+            break;
+        case 30:
+            cout << "Thirty";
+            break;
+        }
     }
-
-    else if( b > a && b > c ) {
-        printf( "%d ", b ) ;
-        if( a > c ) printf( "%d %d ", a, c ) ;
-        else printf( "%d %d ", c,a ) ;
+    else{
+        printf("Error");
     }
-
-    else if( c > b && c > a ) {
-        printf( "%d ", c ) ;
-        if( b > a ) printf( "%d %d ", b,a ) ;
-        else printf( "%d %d ", a,b ) ;
-    }
-
     return 0;
 }
